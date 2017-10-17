4be327f3a77c930cb46309ce675c312650378ff1
Lukas Hermann <lukas.hermann@gmx.de>
2017-10-17 16:07:16 +0200

    ...

--- run_train_jaco.sh
+++ run_train_jaco.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python main.py --env-id Jaco-v1 --kl-desired 0.002 --lr-vf 0.001 --seed 1 --max-timesteps 100000000 --timesteps-per-batch 5000
+python main.py --env-id Jaco-v1 --kl-desired 0.002 --lr-vf 0.001 --seed 1 --max-timesteps 100000000 --timesteps-per-batch 12000
