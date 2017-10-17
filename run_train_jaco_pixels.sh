431f527cfb8d9d298ff3865323411b4355eba16b
Lukas Hermann <lukas.hermann@gmx.de>
2017-10-17 15:08:34 +0200

    kl desired: 0.003

--- run_train_jaco_pixels.sh
+++ run_train_jaco_pixels.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=1 python main.py --env-id JacoPixel-v1 --kl-desired 0.001 --lr-vf 0.0001 --cold-lr-vf 0.0001 --seed 1 --max-timesteps 100000000 --timesteps-per-batch 12000 --use-pixels True --is-rgb True --is-depth True
+CUDA_VISIBLE_DEVICES=2 python main.py --env-id JacoPixel-v1 --kl-desired 0.003 --lr-vf 0.0001 --cold-lr-vf 0.0001 --seed 1 --max-timesteps 100000000 --timesteps-per-batch 12000 --use-pixels True --is-rgb True --is-depth True
